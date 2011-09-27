6c2e9bb7c8f975e79d5c2ab92707f314cba19328
kevincos <kcostello42@gmail.com>
2011-09-26 22:14:28 -0700

    Save Slot Reporting

--- VexedCore/VexedCore/VexedCore/SaveGameText.cs
+++ VexedCore/VexedCore/VexedCore/SaveGameText.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Xml.Serialization;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -11,14 +13,52 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
 namespace VexedCore
-{   
+{
+    public class SaveSummary
+    {
+        public bool empty = true;
+        public bool expertMode = false;
+        public int completionPercentage = 0;
+        public int explorePercentage = 0;
+        public int equipment = 0;
+        public int gameTime = 0;
+    }
 
     public class SaveGameText
     {
         public static RenderTarget2D textRenderTarget;
 
+        public List<SaveSummary> saveSummaryData;
+
         public SaveGameText()
         {
+            saveSummaryData = new List<SaveSummary>();
+            for (int saveSlot = 1; saveSlot < 5; saveSlot++)
+            {
+                SaveSummary summary = new SaveSummary();
+                if (File.Exists("saveFile" + saveSlot))
+                {
+                    Stream stream = new FileStream("saveFile" + saveSlot, FileMode.Open, FileAccess.ReadWrite);
+                    XmlSerializer serializer = new XmlSerializer(typeof(SaveData));
+                    SaveData saveFile = (SaveData)serializer.Deserialize(stream);
+                    stream.Close();
+                    for (int i = 0; i < saveFile.player.upgrades.Length; i++)
+                    {
+                        if(saveFile.player.upgrades[i] == true)
+                        summary.equipment++;
+                    }
+                    int totalOrbs = 0;
+                    int playerOrbs = 0;
+                    foreach (Room r in saveFile.roomList)
+                    {
+                        totalOrbs += r.maxOrbs;
+                        playerOrbs += r.currentOrbs;
+                    }
+                    summary.completionPercentage = 100*playerOrbs / totalOrbs;
+                    summary.empty = false;
+                }
+                saveSummaryData.Add(summary);
+            }
             PresentationParameters pp = Game1.graphicsDevice.PresentationParameters;
             textRenderTarget = new RenderTarget2D(Game1.graphicsDevice,
                                                    256,128, false,
@@ -37,35 +77,31 @@ namespace VexedCore
                 {
                     int saveSlot = 0;
                     if(d.id.Contains("Slot1"))
-                        saveSlot = 1;
+                        saveSlot = 0;
                     if (d.id.Contains("Slot2"))
-                        saveSlot = 2;
+                        saveSlot = 1;
                     if (d.id.Contains("Slot3"))
-                        saveSlot = 3;
+                        saveSlot = 2;
                     if (d.id.Contains("Slot4"))
-                        saveSlot = 4;
+                        saveSlot = 3;
 
-                    if (saveSlot < 3)
-                    {
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Mission Log " + saveSlot, new Vector2(10, 5), Color.YellowGreen);
-                        if(saveSlot == 2)
-                            Engine.spriteBatch.DrawString(Engine.spriteFont, "Difficulty: Expert", new Vector2(10, 25), Color.YellowGreen);
-                        else
-                            Engine.spriteBatch.DrawString(Engine.spriteFont, "Difficulty: Normal", new Vector2(10, 25), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Completion: "+(18*saveSlot) +"%", new Vector2(10, 45), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Map Exploration: " + (12 * saveSlot) + "%", new Vector2(10, 65), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Equipment: " + (3 * saveSlot) + " / 23", new Vector2(10, 85), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Time: 0" + (4 * saveSlot) + ":" + (1 * saveSlot) + "3:0" + (3 * saveSlot) + "", new Vector2(10, 105), Color.YellowGreen);
-                    }
-                    else
-                    {
+                    if(saveSummaryData[saveSlot].empty == true)
                         Engine.spriteBatch.DrawString(Engine.spriteFont, "New Game", new Vector2(10, 5), Color.YellowGreen);
+                    else
+                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Mission Log " + (saveSlot+1), new Vector2(10, 5), Color.YellowGreen);
+
+                    if(saveSummaryData[saveSlot].expertMode == true)
+                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Difficulty: Expert", new Vector2(10, 25), Color.YellowGreen);
+                    else if(saveSummaryData[saveSlot].empty == true)
                         Engine.spriteBatch.DrawString(Engine.spriteFont, "Difficulty: -", new Vector2(10, 25), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Completion: 0%", new Vector2(10, 45), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Map Exploration: 0%", new Vector2(10, 65), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Equipment: 0 / 23", new Vector2(10, 85), Color.YellowGreen);
-                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Time: 00:00:00", new Vector2(10, 105), Color.YellowGreen);
-                    }
+                    else
+                        Engine.spriteBatch.DrawString(Engine.spriteFont, "Difficulty: Normal", new Vector2(10, 25), Color.YellowGreen);
+
+                    Engine.spriteBatch.DrawString(Engine.spriteFont, "Completion: " + saveSummaryData[saveSlot].completionPercentage + "%", new Vector2(10, 45), Color.YellowGreen);
+                    Engine.spriteBatch.DrawString(Engine.spriteFont, "Map Exploration: " + saveSummaryData[saveSlot].explorePercentage + "%", new Vector2(10, 65), Color.YellowGreen);
+                    Engine.spriteBatch.DrawString(Engine.spriteFont, "Equipment: " + saveSummaryData[saveSlot].equipment + " / 23", new Vector2(10, 85), Color.YellowGreen);
+                    Engine.spriteBatch.DrawString(Engine.spriteFont, "Time: " + TimeSpan.FromMilliseconds(saveSummaryData[saveSlot].gameTime), new Vector2(10, 105), Color.YellowGreen);
+                    
                 }
             }
 
